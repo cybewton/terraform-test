aa905abc181940e5b08adcc2d8aa988061e38c4e
Frederic Simard <frederic.simard@dramisinfo.com>
2024-04-14 22:02:29 -0400

    fix count

--- main.tf
+++ main.tf
@@ -15,5 +15,5 @@ resource "azurerm_subnet" "subnet" {
   name                 = "terraform-controller-subnet"
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.${each.value}.0.0/24"]
+  address_prefixes     = ["10.${count.index + 1}.0.0/24"]
 }
