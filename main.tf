6012031eac2461d1a1536c62d9044d5a52222e4e
Frederic Simard <frederic.simard@dramisinfo.com>
2024-04-14 22:06:44 -0400

    Add subnets

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "azurerm_virtual_network" "vnet" {
 }
 
 resource "azurerm_subnet" "subnet" {
-  count                = 3
+  count                = 50
   name                 = "terraform-controller-subnet"
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.vnet.name
