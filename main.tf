509cb21753d202961bbc8be0aa0b0ba2664de328
Frederic Simard <frederic.simard@dramisinfo.com>
2024-04-14 21:59:37 -0400

    create vnet and subnets

--- main.tf
+++ main.tf
@@ -2,3 +2,18 @@ resource "azurerm_resource_group" "example" {
   name     = "terraform-controller-rg"
   location = "canadacentral"
 }
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "terraform-controller-vnet"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  address_space       = ["10.1.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  count                = 3
+  name                 = "terraform-controller-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.${each.value}.0.0/24"]
+}
