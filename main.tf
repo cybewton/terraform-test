3100733cd8b7db19a5e7436de50b4241b4cbbfe9
Frederic Simard <frederic.simard@dramisinfo.com>
2024-04-14 21:53:49 -0400

    test

--- main.tf
+++ main.tf
@@ -1,21 +1,4 @@
-terraform {
-  required_version = ">= 1.3.9"
-}
-
-variable "subject" {
-  type        = string
-  default     = "World"
-  description = "Subject to hello"
-}
-
-resource "random_id" "id" {
-  keepers = {
-    trigger = var.subject
-  }
-
-  byte_length = 4
-}
-
-output "hello_world" {
-  value = "Hello World, ${var.subject} ${random_id.id.hex}!"
+resource "azurerm_resource_group" "example" {
+  name     = "terraform-controller-rg"
+  location = "canadacentral"
 }
