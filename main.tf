5a48a9f0c972035d0a6f9aaddce3a5067bed8bfa
Frederic Simard <frederic.simard@dramisinfo.com>
2024-04-14 22:18:14 -0400

    change subnet name

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "azurerm_virtual_network" "vnet" {
 
 resource "azurerm_subnet" "subnet" {
   count                = 3
-  name                 = "terraform-controller-subnet"
+  name                 = "terraform-controller-subnet-${count.index + 1}"
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.1.${count.index + 1}.0/24"]
